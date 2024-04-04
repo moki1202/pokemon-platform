56e3935a41dce88c72b709104d589f15965b8d9c
Divya Pamecha <21123621+Its-Maniaco@users.noreply.github.com>
2024-04-04 09:44:32 +0530

    removed firebase. added auth page and function using supabase

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,40 +1,43 @@
-'use client'
-import React, { useState } from 'react'
+import React from 'react'
+import SupabaseLogo from '@/components/logo-supabase'
+import { login, signup } from './actions'
 
-const Login: React.FC = () => {
-  console.log('hello')
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    // Handle login logic here
-  }
-
+export default function LoginPage() {
   return (
-    <>
-      <div className='flex text-black'>
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-          <input
-            type='email'
-            placeholder='Email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button type='submit'>Login</button>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="bg-white/10 rounded-md p-6 border border-white/5 w-[300px]">
+        <div className="flex justify-center mb-6 mt-2"><SupabaseLogo/></div>
+        
+        <form className='flex flex-col gap-2'>
+          <Label htmlFor="email">Email:</Label>
+          <Input id="email" name="email" type="email" required />
+          <Label htmlFor="password">Password:</Label>
+          <Input id="password" name="password" type="password" required/>
+          <Separator />
+          <Button formAction={login}>Log in</Button>
+          <Button formAction={signup}>Sign up</Button>
         </form>
       </div>
-    </>
+    </main>
   )
 }
 
-export default Login
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+
+const Button: React.FC<ButtonProps> = ({children, ...rest}) => {
+  return <button className="rounded hover:bg-white hover:text-black font-semibold px-4 py-2 border border-white/20 transition duration-100 ease-in-out" {...rest}>
+    {children}
+  </button>
+}
+
+const Input: React.FC<InputProps> = ({...rest}) => {
+  return <input className="rounded px-4 py-2 text-white outline-none bg-black/30 border border-white/10 focus:bg-white focus:text-black" {...rest}/>
+}
+
+const Label: React.FC<LabelProps> = ({children, ...rest}) => {
+  return <label {...rest} className="text-sm text-white/30">{children}</label>
+}
+
+const Separator = () => <hr className="border-white/10 my-2"/>
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,48 +1,10 @@
 'use client'
 import React, { useState } from 'react';
-import SignUp from '@/src/firebase/auth/signup'; // Assuming SignUp component is in a separate file
 
 const SignupPage: React.FC = () => {
-  const [username, setUsername] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-
-  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  };
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSignup = () => {
-    // Call the signup function from another file with username, email, and password
-    SignUp({ username, email, password }, () => {
-    });
-  };
-
   return (
-    <div className="center-container">
-      <h1>Signup Page</h1>
-      <form className="signup-form" onSubmit={handleSignup}>
-        <div>
-          <label>Username:</label>
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={handleEmailChange} />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={handlePasswordChange} />
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
+    <div>
+      <p>Sign up Page not needed</p>
     </div>
   );
 };
