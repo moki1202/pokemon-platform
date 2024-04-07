00603dcea0fd2492b3f23b36dd27e21f099b63e5
Divya Pamecha <21123621+Its-Maniaco@users.noreply.github.com>
2024-04-07 11:43:05 +0530

    Removed HARDCODED api keys

--- app/auth/supabase/supabase-config.ts
+++ app/auth/supabase/supabase-config.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabaseConfig = createClient(
-  `https://ehbbdhnoflxpuhvgbptn.supabase.co`,
-  `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoYmJkaG5vZmx4cHVodmdicHRuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjMwNTk1NywiZXhwIjoyMDI3ODgxOTU3fQ.trYtGnExZUQnICBzQdxzo2bqdlHwzl37GhgJEqNXWDs`
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
